5740518f2b1ca8e316c9fa76d0da71f94ddc0988
kukovisuals <clubhousenet@live.com>
2022-01-07 21:19:33 -0500

    added redirect in app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import React from 'react';
 import {BrowserRouter as Router,
         Routes,
-        Route} from 'react-router-dom';
+        Route,
+        Navigate } from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import Homepage from './pages/homepage/homepage';
@@ -17,9 +18,9 @@ class App extends React.Component {
   unsubscribeFromAuth = null
  
   componentDidMount(){
+
     const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      console.log(userAuth)
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
@@ -28,7 +29,6 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
           });
-          console.log('current User', setCurrentUser)
         })
       }
       setCurrentUser(userAuth);
@@ -46,14 +46,24 @@ class App extends React.Component {
         <Routes>
           <Route exact path='/' element={<Homepage/>}/>
           <Route exact path='/shop' element={<ShopPage />}/>
-          <Route exact path='/signin' element={<SignInAndOut />}/>
+          <Route exact path='/signin' 
+            element ={ this.props.currentUser ? (
+              <Navigate to ='/'/>
+              ) : (
+              <SignInAndOut/>
+              )} />
         </Routes>
       </div>
     );
   }
 }
+
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch =>({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
